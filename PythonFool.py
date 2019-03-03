4b84b6b3604608677081e25f151c7cc364e42549
JS <43799310+UnboundWarrior@users.noreply.github.com>
2019-03-02 23:57:43 -0600

    Add files via upload

---
+++ PythonFool.py
@@ -0,0 +1,11 @@
+#This program is THE BEST EVER FOLKS
+python = 'Python'
+language = 'Python is the best language.'
+python2 = 'Not python'
+if python == 'Python':
+    print('Python is the best!')
+elif python2 == 'Not Python':
+    if language == 'Python is the best language.':
+        print('Yes, it is.')
+    else:
+        print('No,it is not.')
