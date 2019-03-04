2b8ed9329afc8d405a6fd892d4420ed10ae157a3
Zach <43799310+UnboundWarrior@users.noreply.github.com>
2019-03-03 21:55:37 -0600

    Add files via upload
    Solution to a problem found on (https://www.hackerrank.com/challenges/python-arithmetic-operators/problem)

---
+++ ArithmeticOperatorsProg.py
@@ -0,0 +1,12 @@
+# Our task is to, read two integers and then print out three lines.
+# The first line would consist of: The sum of both integers.
+# The second line gives the difference of the two numbers.
+# The third line gives us the product of the two integers.
+num = int(input("Hand over the number fool! "))
+num2 = int(input("That ain't enough hand over some more fool! "))
+sum = num + num2
+difference = num - num2
+product = num * num2
+print(sum)
+print(difference)
+print(product)
