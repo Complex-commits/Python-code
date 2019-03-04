0a3295d078c40533f7be88408f1c58915a9e541a
Zach <43799310+UnboundWarrior@users.noreply.github.com>
2019-03-03 21:56:53 -0600

    Update ArithmeticOperatorsProg.py

--- ArithmeticOperatorsProg.py
+++ ArithmeticOperatorsProg.py
@@ -2,6 +2,7 @@
 # The first line would consist of: The sum of both integers.
 # The second line gives the difference of the two numbers.
 # The third line gives us the product of the two integers.
+# This problem is found from https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
 num = int(input("Hand over the number fool! "))
 num2 = int(input("That ain't enough hand over some more fool! "))
 sum = num + num2
