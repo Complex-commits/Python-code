b395a94bede41ae579bccd4be1cd15aa18697ab5
Zach <43799310+UnboundWarrior@users.noreply.github.com>
2019-03-04 19:46:56 -0600

    Update DivisonPy.py

--- DivisonPy.py
+++ DivisonPy.py
@@ -1,5 +1,6 @@
 # We have to read two integers that the user will input,and the answer will
 # get an output of a whole integer and also a decimal.
+# This is the solution to the divison problem found here: https://www.hackerrank.com/challenges/python-division/problem
 number1 = int(input("GIMME THE NUMBER FOOL "))
 number2 = int(input("Now....hand over the number and no-one gets hurt. "))
 print(int(number1 / number2))
