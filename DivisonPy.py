fb6f84309f6d85436013fb511a53da4a99257801
Zach <43799310+UnboundWarrior@users.noreply.github.com>
2019-03-04 19:45:56 -0600

    Add files via upload

---
+++ DivisonPy.py
@@ -0,0 +1,6 @@
+# We have to read two integers that the user will input,and the answer will
+# get an output of a whole integer and also a decimal.
+number1 = int(input("GIMME THE NUMBER FOOL "))
+number2 = int(input("Now....hand over the number and no-one gets hurt. "))
+print(int(number1 / number2))
+print(float(number1 / number2))
